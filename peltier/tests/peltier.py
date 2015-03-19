82cc641c40003eec65b45a868ae2c4204540381f
Naveen Narayanan <nnarayanan12@gmail.com>
2015-03-19 02:17:49 +0000

    Updating peltier tests

--- peltier/tests/peltier.py
+++ peltier/tests/peltier.py
@@ -5,31 +5,35 @@ class Peltier:
 	def init():
 		GPIO.setmode(GPIO.BOARD)
 		# warm
-		GPIO.setup(22,GPIO.OUT)
+		GPIO.setup(19,GPIO.OUT)
 		# cool
 		GPIO.setup(24,GPIO.OUT)
-		#enable 
-		GPIO.setup(18,GPIO.OUT)
+		# enable 
+		GPIO.setup(12,GPIO.OUT)
+		# test
+		#GPIO.setup(26,GPIO.OUT)
 
 	@staticmethod
 	def hot():
 		print("hot")
-		GPIO.output(22,GPIO.HIGH)
+		GPIO.output(19,GPIO.LOW)
 		GPIO.output(24,GPIO.HIGH)
-		GPIO.output(18,GPIO.LOW)
+		GPIO.output(12,GPIO.HIGH)
+		#GPIO.output(26,GPIO.LOW)
 
 	@staticmethod
 	def cold():
 		print("cold")
-		GPIO.output(22,GPIO.LOW)
-		GPIO.output(24,GPIO.HIGH)
-		GPIO.output(18,GPIO.HIGH)
+		GPIO.output(19,GPIO.HIGH)
+		GPIO.output(24,GPIO.LOW)
+		GPIO.output(12,GPIO.HIGH)
+                #GPIO.output(26,GPIO.HIGH)
 
 	@staticmethod
 	def stop():
 		print("stop")
-		GPIO.output(22,GPIO.LOW)
+		GPIO.output(19,GPIO.LOW)
 		GPIO.output(24,GPIO.LOW)
-		GPIO.output(18,GPIO.LOW)
+		#GPIO.output(26,GPIO.HIGH)
 		
                 
--- peltier/tests/peltier_test.py
+++ peltier/tests/peltier_test.py
@@ -6,10 +6,10 @@ peltier.Peltier.init()
 
 print "hot"
 peltier.Peltier.hot()
-sleep(5)
+sleep(10)
 
 print "cold"
 peltier.Peltier.cold()
-sleep(5)
+sleep(10)
 
 peltier.Peltier.stop()
