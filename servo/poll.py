24cc2f25b067b4d87c4cac79fae7d33d3653f188
Seanny123 <saubin@uwaterloo.ca>
2015-03-18 23:09:58 -0400

    reading from serial too

---
+++ servo/poll.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+def receiving(ser):
+    global last_received
+
+    buffer_string = ''
+    while True:
+        buffer_string = buffer_string + ser.read(ser.inWaiting())
+        if '\n' in buffer_string:
+            lines = buffer_string.split('\n') # Guaranteed to have at least 2 entries
+            last_received = lines[-2]
+            #If the Arduino sends lots of empty lines, you'll lose the
+            #last filled line, so you could make the above statement conditional
+            #like so: if lines[-2]: last_received = lines[-2]
+            buffer_string = lines[-1]
+            print(last_received)
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+receiving(ser)
